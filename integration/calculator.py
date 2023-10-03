edbe36108381cf34dee695d22f7f1d26ed224223
SumathiMicheal <sumathimicheal6200@conestogac.on.ca>
2023-10-03 19:05:52 -0400

    file added

--- integration/calculator.py
+++ integration/calculator.py
@@ -9,6 +9,6 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     if num2 == 0:
-        raise ValueError("Cannot divide by zero and try again")
+        raise ValueError("Cannot divide by zero and try again1")
     return num1 / num2
 
